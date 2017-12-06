3c790d6a35e0b9bb01b5a8d74a39fddaf7beac75
Artem Abramov <tematibr@gmail.com>
2017-12-06 15:48:06 +0300

    Working dominators

--- dominators.c
+++ dominators.c
@@ -1,7 +1,7 @@
 /*
  * =====================================================================================
  *
- *       Filename:  liveness.c
+ *       Filename:  dominators.c
  *
  *    Description:  
  *
@@ -24,25 +24,13 @@
 #include <math.h>
 
 typedef struct {
-    char shortname[50];
-    char fullname[70];
-    Ins* instruction; // instruction that added this variable
-} Tmps;
-
-typedef struct {
-    // contain the short name for gen/kill and long name for rd
-    Tmps gen[200];
-    int genlen;
-    Tmps kill[200];
-    int killlen;
-    Tmps in[200];
-    int inlen;
-    Tmps use[20];
-    int uselen;
-    Tmps live_out[200];
-    int live_outlen;
-    Tmps live_in[200];
-    int live_inlen;
+    // predeccessor (parent) blocks, that point to this block
+    Blk* papa[200];
+    int papalen;
+    // blocks that this block dominates
+    Blk* d[200];
+    int dlen;
+    bool processed;
 } Meta;
 
 typedef struct {
@@ -63,138 +51,125 @@ Meta* getMeta(Blk *needle) {
     return NULL;
 }
 
-static int findTmpWithShortName(char* short_needle, Tmps* haystack, int hay_len) {
+static bool hasBlk(Blk* needle, Blk** haystack, int hay_len) {
     for (int index = 0; index<hay_len; index++) {
-        if (strcmp(haystack[index].shortname, short_needle) == 0) {
-            return index;
+        if (haystack[index] == needle) {
+            return true;
         }
     }
-    return -1;
-}
-
-static int searchTmpFullName(char* full_needle, Tmps* haystack, int hay_len) {
-    for (int index = 0; index<hay_len; index++) {
-        if (strcmp(haystack[index].fullname, full_needle) == 0) {
-            return index;
-        }
-    }
-    return -1;
-}
-
-static Tmps convertTmp(Blk* blk, Tmp t, Ins* i) {
-    Tmps myvar;
-    // for gen/kill analysis
-    sprintf(myvar.shortname, "%s", t.name);
-    // for rd analysis
-    sprintf(myvar.fullname, "@%s%%%s", blk->name, t.name);
-    myvar.instruction = i;
-    return myvar;
+    return false;
 }
 
 static bool processBlk(Blk* blk) {
     bool change = false;
-    // arrays contain to.val
     Meta* meta = getMeta(blk);
-    Tmps chldin[meta->genlen + meta->inlen];
-    int chldinlen = 0;
-    for (int i=0; i < meta->inlen; i++) {
-        int index = searchTmpFullName(meta->in[i].fullname, meta->kill, meta->killlen);
-        if (index < 0) {
-            // if not found, then this IN var is transitive, send to next blk
-            chldin[chldinlen++] = meta->in[i];
+
+    for (int i = 0; i < meta->papalen; i++) {
+        Blk* parent = meta->papa[i];
+        Meta* parentmeta = getMeta(parent);
+        for (int index=0; index < parentmeta->dlen; index++) {
+            // a candidate for dominating you
+            Blk* parent_dom = parentmeta->d[index];
+            if (hasBlk(parent_dom, meta->d, meta->dlen)) {
+                // you already have this listed as dominating you! (if this is not the first iteration)
+                continue;
+            }
+            // for each block that dominates your parent, check if other parents have it dominating them
+            bool all_parents_have_this_blk = true;
+            for (int k=0; k < meta->papalen; k++) {
+                Blk* other_parent = meta->papa[k];
+                if (other_parent == parent) {
+                    // when your other_parent is the one you checked first, dont check again
+                    continue;
+                }
+                if (other_parent == blk) {
+                    // when your other_parent is yourself
+                    // of course you will never have this blk listed as dominator
+                    continue;
+                }
+                Meta* other_parentmeta = getMeta(other_parent);
+                if (! other_parentmeta->processed) {
+                    // if the other block has not been processed, consider it as no restrictions (anything is ok)
+                    continue;
+                }
+                if (! hasBlk(parent_dom, other_parentmeta->d, other_parentmeta->dlen)) {
+                    // one of the parents does NOT have it, (so that block can NOT dominate you)
+                    all_parents_have_this_blk = false;
+                    break;
+                }
+            }
+            if (all_parents_have_this_blk) {
+                // only if all other parents have this block
+                // and you have not added it already
+                // then add it
+                meta->d[meta->dlen++] = parent_dom;
+                change = true;
+            }
         }
     }
 
-    for (int i=0; i < meta->genlen; i++) {
-        chldin[chldinlen++] = meta->gen[i];
-    }
+    meta->processed = true;
+
+    return change;
+}
+
+
+static void setup(Fn* fn) {
+  metamap = (MapItem*) malloc(fn->nblk * sizeof(MapItem));
+  metamaplen = fn->nblk;
+  metas = (Meta*) malloc(fn->nblk * sizeof(Meta));
+  int mm_i = 0;
+  for (Blk* blk = fn->start; blk; blk = blk->link) {
+    // set len of meta to zero
+    metas[mm_i].papalen = 0;
+    metas[mm_i].dlen = 0;
+    metas[mm_i].processed = false;
+    // connect meta to block
+    metamap[mm_i].key = blk;
+    metamap[mm_i].value = &metas[mm_i];
+    // for every block
+    mm_i++;
+  }
+}
+
+static bool buildParents(Blk* blk) {
+    bool change = false;
 
     if (blk->s1) {
         Blk* lchild = blk->s1;
         Meta* lmeta = getMeta(lchild);
-        // while we have out genvars add them to next block meta
-        for (int i=0; i < chldinlen; i++) {
-            int index = searchTmpFullName(chldin[i].fullname, lmeta->in, lmeta->inlen);
-            if (index < 0) {
-                // something new
-                lmeta->in[lmeta->inlen++] = chldin[i];
-                change = true;
-            }
-        }
+        // add yourself to your childs parents array
+        lmeta->papa[lmeta->papalen++] = blk;
+        change = true;
     }
 
     if (blk->s2) {
         Blk* rchild = blk->s2;
         Meta* rmeta = getMeta(rchild);
-        // while we have out genvars add them to next block meta
-        for (int i=0; i < chldinlen; i++) {
-            int index = searchTmpFullName(chldin[i].fullname, rmeta->in, rmeta->inlen);
-            if (index < 0) {
-                // something new
-                rmeta->in[rmeta->inlen++] = chldin[i];
-                change = true;
-            }
-        }
+        // add yourself to your childs parents array
+        rmeta->papa[rmeta->papalen++] = blk;
+        change = true;
     }
+
     return change;
 }
 
-static void genKill(Fn* fn) {
-  for (Blk *blk = fn->start; blk; blk = blk->link) {
-    // arrays contain to.val
+
+static void readfn (Fn *fn) {
+  // setup for analysis
+  setup(fn);
+
+  // setup every block to dominate itself
+  for (Blk* blk = fn->start; blk; blk = blk->link) {
     Meta* meta = getMeta(blk);
-
-    for (Ins* i=blk->ins; i-blk->ins < blk->nins; i++) {
-        // if we are assigning to a temporary variable and not elsewhere
-        if (Tmp0 > i->to.val) {
-            continue;
-        }
-        char* s1 = fn->tmp[i->to.val].name;
-        int index = findTmpWithShortName(s1, meta->gen, meta->genlen);
-        if (index >= 0) {
-            // its not unique
-            continue;
-        }
-        meta->gen[meta->genlen++] = convertTmp(blk, fn->tmp[i->to.val], i);
-        // then we are GEN the variable in this block
-    }
-
-    // count down from gen_count
-    int k = meta->genlen;
-    while (k-- > 0) {
-        // search where we might be KILL this variable
-        // iterate over all other blocks and instructions
-        Tmps vnew = meta->gen[k];
-        for (Blk *blk_other = fn->start; blk_other; blk_other = blk_other->link) {
-            if (blk_other == blk) {
-                // this is the same block as current
-                continue;
-            }
-            for (Ins* i_other=blk_other->ins; i_other-blk_other->ins < blk_other->nins; i_other++) {
-                if (Tmp0 > i_other->to.val) {
-                    // not a var (assignement)
-                    continue;
-                }
-                Tmps vnew_other = convertTmp(blk_other, fn->tmp[i_other->to.val], i_other);
-                if (strcmp(vnew.shortname, vnew_other.shortname) != 0) {
-                    // the other assignement instruction is for another variable
-                    continue;
-                }
-                int index = searchTmpFullName(vnew_other.fullname, meta->kill, meta->killlen);
-                if (index >= 0) {
-                    // already killed THIS var in THIS block
-                    continue;
-                }
-                meta->kill[meta->killlen++] = convertTmp(blk_other, fn->tmp[i_other->to.val], i_other);
-                // then we killed that instruction, that assigned to the same variable
-            }
-        }
-    }
+    meta->d[meta->dlen++] = blk;
   }
 
-}
+  // build lists of parents (reverse the graph)
+  for (Blk* blk = fn->start; blk; blk = blk->link) {
+    buildParents(blk);
+  }
 
-static void reaching(Fn* fn) {
   bool change = true;
   while (change) {
     change = false;
@@ -204,142 +179,18 @@ static void reaching(Fn* fn) {
         }
     }
   }
-}
-
-static void setup(Fn* fn) {
-  metamap = (MapItem*) malloc(fn->nblk * sizeof(MapItem));
-  metamaplen = fn->nblk;
-  metas = (Meta*) malloc(fn->nblk * sizeof(Meta));
-  int mm_i = 0;
-  for (Blk* blk = fn->start; blk; blk = blk->link) {
-    // set len of meta to zero
-    metas[mm_i].inlen = 0;
-    metas[mm_i].genlen = 0;
-    metas[mm_i].killlen = 0;
-    metas[mm_i].uselen = 0;
-    // connect meta to block
-    metamap[mm_i].key = blk;
-    metamap[mm_i].value = &metas[mm_i];
-    // for every block
-    mm_i++;
-  }
-}
-
-static void defUse(Fn* fn) {
-  // need to count uses
-  for (Blk* blk = fn->start; blk; blk = blk->link) {
-    Meta* meta = getMeta(blk);
-    for (Ins* i=blk->ins; i-blk->ins < blk->nins; i++) {
-        for (int argc = 0; argc < 2; argc++) {
-            // struct Alias { ABot, ALoc ...
-            if (Tmp0 <= i->arg[argc].val && i->arg[argc].type <= 1) {
-                // if the arg is a variable
-                Tmp t = fn->tmp[i->arg[argc].val];
-                int index_gen = findTmpWithShortName(t.name, meta->gen, meta->genlen);
-                if (index_gen >= 0 && meta->gen[index_gen].instruction < i) {
-                    // we gen it in the same block as use it, does not count
-                    // AND we gen it earlier than use it
-                    continue;
-                }
-                int index_use = findTmpWithShortName(t.name, meta->use, meta->uselen);
-                if (index_use >= 0) {
-                    // already have this use
-                    continue;
-                }
-                meta->use[meta->uselen++] = convertTmp(blk, fn->tmp[i->arg[argc].val], i);
-            }
-        }
-    }
-  }
-}
-
-static bool analyzeBlock(Blk* blk) {
-    bool change = false;
-    Meta* meta = getMeta(blk);
-
-    if (blk->s1) {
-        Meta* meta_s1 = getMeta(blk->s1);
-        for (int i=0; i < meta_s1->live_inlen; i++) {
-            // add to live out for your block
-            int index = findTmpWithShortName(meta_s1->live_in[i].shortname, meta->live_out, meta->live_outlen);
-            // if we have NOT added it already
-            if (index < 0) {
-                meta->live_out[meta->live_outlen++] = meta_s1->live_in[i];
-                change = true;
-            }
-        }
-    }
-
-    if (blk->s2) {
-        Meta* meta_s2 = getMeta(blk->s2);
-        for (int i=0; i < meta_s2->live_inlen; i++) {
-            // add to live out for your block
-            int index = findTmpWithShortName(meta_s2->live_in[i].shortname, meta->live_out, meta->live_outlen);
-            // if we have NOT added it already
-            if (index < 0) {
-                meta->live_out[meta->live_outlen++] = meta_s2->live_in[i];
-                change = true;
-            }
-        }
-    }
-
-    for (int i=0; i < meta->live_outlen; i++) {
-        int index = findTmpWithShortName(meta->live_out[i].shortname, meta->gen, meta->genlen);
-        if (index >= 0) {
-            // if you dont def it in this block
-            continue;
-        }
-        int index2 = findTmpWithShortName(meta->live_out[i].shortname, meta->live_in, meta->live_inlen);
-        if (index2 >= 0) {
-            // already have this
-            continue;
-        }
-        meta->live_in[meta->live_inlen++] = meta->live_out[i];
-        change = true;
-    }
-
-    return change;
-}
-
-static void readfn (Fn *fn) {
-  // setup for analysis
-  setup(fn);
-  // gen kill analysis
-  genKill(fn);
-  // reaching definitions analysis
-  reaching(fn);
-  // defuse analysis
-  defUse(fn);
-
-  // setup initial values for live_in
-  for (Blk* blk = fn->start; blk; blk = blk->link) {
-    Meta* meta = getMeta(blk);
-    for (int i = 0; i < meta->uselen; i++) {
-        meta->live_in[meta->live_inlen++] = meta->use[i];
-    }
-  }
-
-  bool change = true;
-  while (change) {
-    change = false;
-    for (Blk* blk = fn->start; blk; blk = blk->link) {
-        if( analyzeBlock(blk) ) {
-            change = true;
-        }
-    }
-  }
 
   for (Blk* blk = fn->start; blk; blk = blk->link) {
-    printf("@%s", blk->name);
-    printf("\n\tlv_out =");
-    // what have we created and overriden, the final list, this is for the meta of the next block
-    Meta* meta = getMeta(blk);
-    for (int i=0; i < meta->live_outlen; i++) {
-        printf(" %%%s", meta->live_out[i].shortname);
+    printf("@%s\t", blk->name);
+    for (Blk* other_blk = fn->start; other_blk; other_blk = other_blk->link) {
+        Meta* other_meta = getMeta(other_blk);
+        if (hasBlk(blk, other_meta->d, other_meta->dlen)) {
+            // if other_blk has this blk as dominator (in the dom array)
+            printf(" @%s", other_blk->name);
+        }
     }
     printf("\n");
   }
-  printf("\n");
 
   free(metamap);
   free(metas);
